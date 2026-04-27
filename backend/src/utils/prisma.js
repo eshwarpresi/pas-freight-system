b58da05c68c3b604536868d6c2047bf9c085c6a8
eshwarpresi <eshwarpresi@gmail.com>
2026-04-27 15:04:42 +0530

    Use public PostgreSQL URL

--- backend/src/utils/prisma.js
+++ backend/src/utils/prisma.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 
-const databaseUrl = process.env.DATABASE_URL || 'postgresql://postgres:sxBQLXpYkJNQFwnKUxMzZlHICroephol@shuttle.proxy.rlwy.net:30071/railway';
+const databaseUrl = process.env.DATABASE_URL;
 
 const prisma = new PrismaClient({
   datasources: {
