caf2bff0ba963107969eb64fee21c49125389627
eshwarpresi <eshwarpresi@gmail.com>
2026-04-27 15:01:09 +0530

    Hardcode DATABASE_URL for Railway

--- backend/src/utils/prisma.js
+++ backend/src/utils/prisma.js
@@ -1,5 +1,13 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+const databaseUrl = process.env.DATABASE_URL || 'postgresql://postgres:sxBQLXpYkJNQFwnKUxMzZlHICroephol@shuttle.proxy.rlwy.net:30071/railway';
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl
+    }
+  }
+});
 
 module.exports = prisma;
