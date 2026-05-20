60dc86d8718a317da3b7ace620c0ac10e9dc5d7c
eshwarpresi <eshwarpresi@gmail.com>
2026-05-20 16:41:49 +0530

    Final: Remove refno call, app stable

--- backend/src/routes/freightForwarding.routes.js
+++ backend/src/routes/freightForwarding.routes.js
@@ -20,9 +20,6 @@ router.delete('/shipments/:id', freightController.deleteShipment);
 // DELETE - All Shipments
 router.delete('/shipments', freightController.deleteAllShipments);
 
-// UPDATE - Reference Number
-router.put('/shipments/:id/refno', freightController.updateRefNo);
-
 // UPDATE - Rates
 router.put('/shipments/:id/rates', freightController.updateRates);
 
