5209cf37f7eaf01903af7aec73a6f69175ca66cf
eshwarpresi <eshwarraju2002@gmail.com>
2026-09-25 17:50:37 +0530

    Fix missing X icon import that crashed CreateShipment.jsx

--- frontend/src/pages/CreateShipment.jsx
+++ frontend/src/pages/CreateShipment.jsx
@@ -7,7 +7,7 @@ import { useSocket } from '../App'
 import { 
   ArrowLeft, Hash, Calendar, Box, User, Anchor, 
   Ship, Sparkles, Loader2, Building2, Globe, AlertCircle,
-  FileCheck, ArrowUpDown, Barcode, Weight, Info, Pencil, Eye, Scale, Mail, Truck, MapPin, ClipboardList, FileText, Plus
+  FileCheck, ArrowUpDown, Barcode, Weight, Info, Pencil, Eye, Scale, Mail, Truck, MapPin, ClipboardList, FileText, Plus, X
 } from 'lucide-react'
 
 const DRAFT_KEY = 'pas_shipment_draft'
